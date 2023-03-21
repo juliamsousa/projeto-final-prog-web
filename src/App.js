b706a7b767fed0877edcd9b536644529a5cc9a95
Júlia Miranda <juliaeduardasousa@gmail.com>
2023-03-21 16:04:19 -0300

    Rotas adicionadas a aplicação

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import AppRoutes from "./routes";
-
-import Home from "./pages/Home/Home";
-import About from "./pages/About/About";
-import Decom from "./pages/Decom/Decom";
 
 function App() {
   return (
-    <Home/>
-    // <About/>
-    // <Decom/>
+    <>
+    </>
   );
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,28 @@
-import React from "react";
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Decom from "./pages/Decom/Decom";
 
-const AppRoutes = () => {
-   return(
-       <BrowserRouter>
-            <Routes>
-                <Route component = { Home }  path="/" exact />   
-                <Route component = { About }  path="/decom" />   
-            </Routes>
-       </BrowserRouter>
-   )
-}
 
-export default AppRoutes;
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+    {
+        path: "/noticias",
+        element: <About/>, 
+    },
+    {
+        path: "/inicio",
+        element: <Decom/>, 
+    },
+    {
+        path: "/*",
+        element: <Home/>, 
+    }
+  ]);
