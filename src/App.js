9c0cf49bb76f2335a73275fe2cf15eb5dcce118e
Júlia Miranda <juliaeduardasousa@gmail.com>
2023-03-24 18:40:21 -0300

    Attempt to deploy (again) :/

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import {RouterProvider} from "react-router-dom";
+import {router} from "./routes"
+
 
 function App() {
   return (
     <>
-      <head>
-        <title>DECOM</title>
-      </head>
+      <RouterProvider router={router} basename="/React"/>
     </>
   );
 }
